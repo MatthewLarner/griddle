b2f37d8f6d36ed9b9e6034adc496a8ea723f57e2
iamalarner <matt.larner.dev@gmail.com>
2015-07-18 22:38:10 +1000

    cell test

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@ var test = require('tape'),
 test('correct number of rows and columns on init', function(t) {
     t.plan(2);
 
-    var grid = new Griddle(2,3);
+    var grid = new Griddle(2, 3);
     t.equal(grid._grid.length, 2);
     t.equal(grid._grid[0].length, 3);
 });
@@ -12,7 +12,7 @@ test('correct number of rows and columns on init', function(t) {
 test('correct number of rows and columns on set', function(t) {
     t.plan(2);
 
-    var grid = new Griddle(5,5);
+    var grid = new Griddle(5, 5);
 
     t.equal(grid.rows(2)._grid.length, 2);
     t.equal(grid.columns(3)._grid[0].length, 3);
@@ -21,7 +21,7 @@ test('correct number of rows and columns on set', function(t) {
 test('get correct row', function(t) {
     t.plan(1);
 
-    var grid = new Griddle(2,2),
+    var grid = new Griddle(2, 2),
         data = [[2,3],[4,5]],
         expectedResult = [4,5];
 
@@ -32,7 +32,7 @@ test('get correct row', function(t) {
 test('set row', function(t) {
     t.plan(2);
 
-    var grid = new Griddle(2,2),
+    var grid = new Griddle(2, 2),
         row = [1,2];
 
     t.deepEqual(grid.row(1, row).row(1), row);
@@ -42,10 +42,21 @@ test('set row', function(t) {
 test('get correct column', function(t) {
     t.plan(1);
 
-    var grid = new Griddle(2,2),
+    var grid = new Griddle(2, 2),
         data = [[2,3],[4,5]],
         expectedResult = [3,5];
 
     grid._grid = data;
     t.deepEqual(grid.column(1), expectedResult);
+});
+
+test('get cell', function(t) {
+    t.plan(1);
+
+    var grid = new Griddle(2, 2),
+        data = {
+            a: 1
+        };
+
+    t.equal(grid.cell(1, 1, data).cell(1,1 ), data);
 });
